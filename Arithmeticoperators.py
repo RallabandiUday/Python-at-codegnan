8edaf552c7a98c8d483f342bfd629da3eb8a70ac
Rallabandi Uday <udayralabandi99@gmail.com>
2026-03-31 19:49:02 +0530

    Add files via upload

---
+++ Arithmeticoperators.py
@@ -0,0 +1,16 @@
+a=10
+b=5
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a//b
+i=a**b
+print("Addition of two numbers ",c)
+print("Subtraction of two numbers ",d)
+print("Multiplication of two numbers ",e)
+print("Quotient of two numbers",f)#result will returns  quotient in float datatype
+print("Module of two numbers",g)#returns remqinder
+print("Quotient of two numbers in int datatype",h)#return quotient but in int data type
+print("Exponent of two numbers ",i)
