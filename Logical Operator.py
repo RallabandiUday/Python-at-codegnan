0a1611aa51d990f5a064d882c6c6c62ac9692aa3
Rallabandi Uday <udayralabandi99@gmail.com>
2026-04-01 20:08:34 +0530

    Add files via upload

---
+++ Logical Operator.py
@@ -0,0 +1,13 @@
+a=20
+b=35
+c=65
+d=0
+print(a and b)
+print(a or c)
+print(c or d)
+print("Logical AND  Operator:",a<b and a>c)
+print("Logical AND Operator:",a<b and b<c)
+print("Logical OR Operator:", a<b or a< c)
+print("Logical OR Operator:", a>b or a> c)
+print("Logical NOT Operator:",  not a> c)
+
