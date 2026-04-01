2bab653555cdaac65e4594618f4d5fdcebc769e2
Rallabandi Uday <udayralabandi99@gmail.com>
2026-04-01 20:08:00 +0530

    Add files via upload

---
+++ Assignmentoperator.py
@@ -0,0 +1,18 @@
+#Assignment operations
+a=b=c=d=e=f=g=10
+a+=10
+b-=2
+c*=5
+d/=2
+e%=5
+f//=2
+g**=2
+print("Addition Assignment is",a)
+print("Subtraction Assignment is",b)
+print("Multiplication Assignment is",c)
+print("float division Assignment is",d)
+print("module Assignment is",e)
+print("division Assignment is",f)
+print("exponential Assignment is",g)
+
+
