95916b8cfbd625e8cb14c1982867a817da2e87cf
Rallabandi Uday <udayralabandi99@gmail.com>
2026-04-01 20:28:47 +0530

    Add files via upload

---
+++ relational operator.py
@@ -0,0 +1,13 @@
+a=105
+b=55
+c=600
+d=600
+print(" operation (a>b) ", a,b,a>b)
+print(" operation (a<b) ", a,b,a<b)
+print(" operation (a>=c) ", a,c,a>=c)
+print(" operation (b<=c) ", b,c,b<=c)
+print(" operation (a==b) ", a,b,a==b)
+print(" operation (a!=b) ", a,b,a!=b)
+print(" operation (c==d) ", c,d,c==d)
+
+
