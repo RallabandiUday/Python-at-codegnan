0c8802c2dfb6ad4a7bf9b03541a8bd946713269c
Rallabandi Uday <udayralabandi99@gmail.com>
2026-04-01 20:09:16 +0530

    Add files via upload

---
+++ Membership and Identity Operator.py
@@ -0,0 +1,12 @@
+a=[2,3,5,6,98]
+b=25
+c=25
+d=125
+print("membership  IN operator:",78 in a)
+print("membership  IN operator:",98 in a)
+print("membership  Notin operator:",78 in a)
+print("membership  Notin operator:",6 in a)
+print("Identity  IS operator:",b is c)
+print("Identity  ISnot operator:",b is not d)
+print("Identity  IS operator:",b is d)
+
