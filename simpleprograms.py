84a38f89295a8a70dec7451457a99f410c214ea4
Rallabandi Uday <udayralabandi99@gmail.com>
2026-04-03 23:10:49 +0530

    Add files via upload

---
+++ simpleprograms.py
@@ -0,0 +1,41 @@
+print("Adding of two numbers")
+a=int(input("enter number"))
+b=int(input("enter number"))
+print(a+b)
+
+
+print("To find quotient and remainder")
+a=int(input("enter number"))
+b=int(input("enter number"))
+c=a%b
+d=a//b
+print("Quotient is:",c)
+print("Remainder is:",d)
+
+print("To calculate the square of number")
+a=int(input("enter number"))
+print("exponent of a number is:",a**2)
+
+
+print("to check that first number is div by second number or not")
+a=int(input("enter number"))
+b=int(input("enter number"))
+if a%b==0:
+      print("divisible ")
+else:
+      print("Not divisible")
+
+
+print("Calculate area of rectangle")
+a=int(input("enter number"))
+b=int(input("enter number"))
+print("Area of rectangle is",a*b)
+
+
+print("To check two numbers are equal or not")
+a=int(input("enter number"))
+b=int(input("enter number"))
+if a==b:
+      print("Two numbers are equal",a,b)
+else:
+    print("Two numbers are not equal",a,b)
---
+++ swappingwithout3rdvariable.py
@@ -0,0 +1,7 @@
+a=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+print("Before swapping:",a,b)
+a=a+b
+b=a-b
+a=a-b
+print("after swapping:",a,b)
